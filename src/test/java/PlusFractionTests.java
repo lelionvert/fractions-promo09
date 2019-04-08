fe8b587aa0ce52eda1dcccc151b959ad8018beb1
kevingoilard <kevin.goilard@lacombedulionvert.fr>
2019-04-08 15:47:16 +0200

    zero plus fraction case

--- src/test/java/PlusFractionTests.java
+++ src/test/java/PlusFractionTests.java
@@ -16,4 +16,13 @@ public class PlusFractionTests {
 
         Assert.assertEquals(fractionZero, result);
     }
+
+    @Test
+    public void zero_plus_fraction() {
+        Fraction fractionZero = new Fraction(0,1);
+        Fraction fraction  = new Fraction(1, 3);
+        Fraction result = fractionZero.add(fraction);
+
+        Assert.assertEquals(fraction, result);
+    }
 }
