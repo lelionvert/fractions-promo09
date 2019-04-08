c8240d9bff14c274f9d3bf9349281b5a80bf4310
kevingoilard <kevin.goilard@lacombedulionvert.fr>
2019-04-08 16:42:21 +0200

    Refactoring fraction plus zero case

--- src/main/java/Fraction.java
+++ src/main/java/Fraction.java
@@ -2,14 +2,14 @@ public class Fraction {
 
     private final int numerator;
 
-    public Fraction(int i, int i1) {
-
-        numerator = i;
+    public Fraction(int numerator, int i1) {
+        this.numerator = numerator;
     }
 
     public Fraction add(Fraction fraction) {
         if (fraction.numerator == 0)
             return this;
+
         return fraction;
     }
 }
