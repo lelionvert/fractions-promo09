c5208830ef87f6e3002250e2d9fe4f62fb779265
kevingoilard <kevin.goilard@lacombedulionvert.fr>
2019-04-08 15:12:13 +0200

    kernel case

---
+++ src/main/java/Fraction.java
@@ -0,0 +1,8 @@
+public class Fraction {
+    public Fraction(int i, int i1) {
+    }
+
+    public Fraction add(Fraction fraction) {
+        return fraction;
+    }
+}
--- src/test/java/FractionTests.java
+++ src/test/java/FractionTests.java
@@ -1,10 +1,19 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Queue;
+
 public class FractionTests {
 
     @Test
-    public void tesst() {
+    public void test() {
         Assert.assertTrue(true);
+    }
+
+    @Test
+    public void kernel_test()
+    {
+        Fraction fraction= new Fraction(0,1);
+        Assert.assertEquals(fraction,fraction.add(fraction));
     }
 }
