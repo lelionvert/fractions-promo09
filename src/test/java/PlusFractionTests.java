7205c7411a5ab1f197aba0286608bb99b9d8a351
kevingoilard <kevin.goilard@lacombedulionvert.fr>
2019-04-09 12:02:12 +0200

    Refactoring tests -> rearrange expected variable

--- src/test/java/PlusFractionTests.java
+++ src/test/java/PlusFractionTests.java
@@ -40,20 +40,20 @@ public class PlusFractionTests {
     @Test
     public void integer_plus_integer() {
         Fraction fraction = new Fraction(1, 1);
-        Fraction expected = new Fraction(2, 1);
 
         Fraction result = fraction.add(fraction);
 
+        Fraction expected = new Fraction(2, 1);
         Assert.assertEquals(expected.getNumerator(), result.getNumerator());
     }
 
     @Test
     public void fraction_plus_fraction_with_same_denominator() {
         Fraction fraction = new Fraction(1, 3);
-        Fraction expected = new Fraction(2, 3);
 
         Fraction result = fraction.add(fraction);
 
+        Fraction expected = new Fraction(2, 3);
         Assert.assertEquals(expected.getNumerator(), result.getNumerator());
         Assert.assertEquals(expected.getDenominator(), result.getDenominator());
     }
