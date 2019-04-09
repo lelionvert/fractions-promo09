eb61f380cb90a5ca9e5a16601a364bad0f77d20a
kevingoilard <kevin.goilard@lacombedulionvert.fr>
2019-04-09 10:50:48 +0200

    Refactoring zero plus fraction

--- src/test/java/PlusFractionTests.java
+++ src/test/java/PlusFractionTests.java
@@ -10,7 +10,7 @@ public class PlusFractionTests {
 
     @Test
     public void zero_plus_zero() {
-        Fraction fractionZero = new Fraction(0,1);
+        Fraction fractionZero = new Fraction(0, 1);
 
         Fraction result = fractionZero.add(fractionZero);
 
@@ -19,12 +19,12 @@ public class PlusFractionTests {
 
     @Test
     public void zero_plus_fraction() {
-        Fraction fractionZero = new Fraction(0,1);
-        Fraction fraction  = new Fraction(1, 3);
+        Fraction fractionZero = new Fraction(0, 1);
+        Fraction fraction = new Fraction(1, 3);
 
         Fraction result = fractionZero.add(fraction);
 
-        Assert.assertEquals(fraction, result);
+        Assert.assertEquals(fraction.getNumerator(), result.getNumerator());
     }
 
     @Test
