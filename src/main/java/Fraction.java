61bb8b51b4f829ee38f3f3596d9de848ecf15157
kevingoilard <kevin.goilard@lacombedulionvert.fr>
2019-04-11 11:51:01 +0200

    refactor fraction times two case

--- src/main/java/Fraction.java
+++ src/main/java/Fraction.java
@@ -50,8 +50,9 @@ public class Fraction {
     }
 
     public Fraction times(Fraction fraction) {
-        if (this.denominator == 3)
+        if (this.denominator == 3) {
             return new Fraction(2, 3);
+        }
         if (this.numerator == 3 || fraction.numerator == 2) {
             return new Fraction(this.numerator * fraction.numerator, 1);
         }
