48af6a80c6b657640d03aa1de038ffefd869e289
kevingoilard <kevin.goilard@lacombedulionvert.fr>
2019-04-11 10:06:30 +0200

    fraction zero times one equals zero case

--- src/test/java/MultiplicationFractionTest.java
+++ src/test/java/MultiplicationFractionTest.java
@@ -11,4 +11,14 @@ public class MultiplicationFractionTest {
 
         assertThat(result).isEqualTo(fractionZero);
     }
+
+    @Test
+    public void zero_times_one_return_zero() {
+        Fraction fractionZero = new Fraction(0, 1);
+        Fraction fraction = new Fraction(1, 1);
+
+        Fraction result = fractionZero.times(fraction);
+
+        assertThat(result).isEqualTo(fractionZero);
+    }
 }
